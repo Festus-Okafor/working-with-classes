45f1f1d5775751c1e19c75803b0f8ef663a3a0c4
mexie07 <okaforfestus07@gmail.com>
2025-05-28 10:55:02 -0400

    new morning problem on classes...

--- files.js
+++ files.js
@@ -1,4 +1,4 @@
- //part 1. playing around with classes
+/*  //part 1. playing around with classes
  class Cat {
   constructor(age, color, sex) { //the parameters here serves as placeholder for the features of the about to be created instances
     this.age = age;
@@ -102,5 +102,79 @@ console.log(`........................................................`)
 
 
 
+      // morning code problems.... dealing with classes
 
+ */
 
+class Hamster {
+  constructor(owner, name, price) {
+    this.owner = owner,
+     this.name = name, 
+     this.price = price;
+  }
+  wheelRun() {
+    console.log(`squeak squeak`);
+  }
+  eatFood() {
+    console.log(`nibble nibble`);
+  }
+  getPrice() {
+       let price = 40;   
+    };
+  }
+
+class Person {
+  constructor(name, age, height, weight, mood, hamsters, bankAccount) {
+    this.name = name,
+      this.age = 0,
+      this.height = 0,
+      this.weight = 0,
+      this.mood = 0,
+      this.hamsters = [],
+      this.bankAccount = 0;
+  }
+  getName() {
+    return this.name;
+  }
+  getAge() {
+    return this.age;
+  }
+  getWeight() {
+    return this.weight;
+  }
+  greet() {
+    console.log(`This is the student information+ ${this.name}`);
+  }
+  eat() {
+    this.weight++;
+    this.mood++;
+  }
+  exercise() {
+    this.weight--;
+  }
+  ageUp() {
+    this.age++;
+    this.height++;
+    this.weight++;
+    this.mood--;
+    this.bankAccount + 10;
+  }
+  buyHamster(hamster) {
+    this.hamsters.push(hamster);
+    this.mood+10;
+    this.bankAccount-hamster.getPrice;
+  }
+}
+
+const Timmy = new Person("Timmy", 5)
+const Gus = new Hamster("Timmy")
+
+Timmy.eat(5)
+Timmy.exercise(5)
+Timmy.ageUp(9)
+Timmy.buyHamster("Gus")
+Timmy.ageUp(15)
+Timmy.eat(2)
+Timmy.exercise*(2)
+
+console.log
